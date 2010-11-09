e449e1ed596e41db360701f431a8f74be80c45c7
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2010-11-09 16:58:55 +0100

    Rspec2 doesn't include logger anymore

--- spec/database.rb
+++ spec/database.rb
@@ -1,7 +1,8 @@
+require 'logger'
 gem 'sqlite3-ruby'
 
 
-ActiveRecord::Base.logger = Logger.new('/tmp/zero_jobs_test.log')
+# ActiveRecord::Base.logger = Logger.new('/tmp/zero_jobs_test.log')
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => '/tmp/zero_jobs_test.sqlite')
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.default_timezone = :utc if Time.zone.nil?
