cc5294057a69e9fe017b69c2858ab7bbc4546a2e
Stéphane Akkaoui <stephane.akkaoui@imeuble.info>
2010-11-09 13:14:07 +0100

    Add ZeroJob::Job object with basic dumping options

--- lib/zero_jobs.rb
+++ lib/zero_jobs.rb
@@ -1,1 +1,5 @@
 # ZeroJobs
+autoload :ActiveRecord, 'activerecord'
+
+require 'zero_jobs'
+require File.dirname(__FILE__) + '/zero_jobs/job'
--- spec/job_spec.rb
+++ spec/job_spec.rb
@@ -1,7 +1,54 @@
+require File.dirname(__FILE__) + '/database'
 require "spec_helper"
 
-describe "atest" do
-  it "should run specs" do
-    1.should == 1
+class SampleObject < ActiveRecord::Base
+end
+
+describe ZeroJobs::Job do
+  before(:each) do
+    @job = ZeroJobs::Job.new
+  end
+  
+  it "should transform class to string" do
+    result = @job.class_to_string(SampleObject)
+    result.class.should == String
+    lambda do
+      json_result = JSON.parse(result)
+      json_result.should == {"class_name" => "SampleObject", "type" => "class"}
+    end.should_not raise_error
+  end
+  
+  it "should transform ActiveRecord instace to JSON string" do
+    sample_object = SampleObject.create(:count => 42)
+    result = @job.active_record_instance_to_string(sample_object)
+    result.class.should == String
+    
+    lambda do
+      json_result = JSON.parse(result)
+      json_result.should == {"class_name" => "SampleObject", "id" => 1, "type" => "active_record_instace"}
+    end.should_not raise_error
+  end
+  
+  it "should raise when trying to dump new_record" do
+    sample_object = SampleObject.new(:count => 42)
+    lambda do
+      @job.active_record_instance_to_string(sample_object)
+    end.should raise_error(ZeroJobs::Job::DumpError)
+  end
+  
+  it "should dump object in string for an instance" do
+    sample_object = SampleObject.create(:count => 2)
+    @job.should_receive(:active_record_instance_to_string)
+    @job.dump(sample_object)
+  end
+
+  it "should dump object in string for a class" do
+    sample_object = SampleObject
+    @job.should_receive(:class_to_string)
+    @job.dump(sample_object)
+  end
+  
+  it "should have an object" do
+    @job.object.should be_nil
   end
 end
